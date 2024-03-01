c774356b6719648d29ecc94610dc0f288cc0f75e
AMOS-D900 <dorathymabieam@gmail.com>
2024-02-29 17:57:51 -1200

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const countdownDate = new Date(" March 7, 2024 11:30:00");
+const countdown = setInterval(function () {
+    const now = new Date();
+    const distance = countdownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}, 1000);
